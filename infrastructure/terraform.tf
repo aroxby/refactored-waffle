1318ca54cf8eeae6adc3a2b161fbaea710b7b651
Andy Roxby <aroxby@users.noreply.github.com>
2023-06-22 14:46:17 -0400

    Add missing provider name

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -16,7 +16,7 @@ provider "aws" {
   region = var.region
 }
 
-provider "" {
+provider "kubernetes" {
   host                   = module.eks.cluster.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster.cluster_certificate_authority_data)
   exec {
