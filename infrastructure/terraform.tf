babb6b211a184cbda9ade483592022953685a4ab
Andy Roxby <aroxby@users.noreply.github.com>
2023-06-26 10:37:53 -0400

    Configure Helm provider

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -5,6 +5,11 @@ terraform {
       version = ">= 4.46.0, < 5.0.0"
     }
 
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 1.0, < 3.0"
+    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.16.1"
@@ -14,6 +19,23 @@ terraform {
 
 provider "aws" {
   region = var.region
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster.cluster_name
+      ]
+    }
+  }
 }
 
 provider "kubernetes" {
