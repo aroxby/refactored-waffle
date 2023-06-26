5ee84530fbd3b015d23adf879e7764079fc489b3
Andy Roxby <aroxby@users.noreply.github.com>
2023-06-26 10:17:52 -0400

    Do not mix cluster name and cluster ID
    This appears to be an error in the example usage.

--- infrastructure/modules/eks/main.tf
+++ infrastructure/modules/eks/main.tf
@@ -89,7 +89,7 @@ module "cloudwatch_logs" {
 
   enabled = true
 
-  cluster_name                     = module.eks.cluster_id
+  cluster_name                     = module.eks.cluster_name
   cluster_identity_oidc_issuer     = module.eks.cluster_oidc_issuer_url
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
   worker_iam_role_name             = module.eks.cluster_iam_role_name
