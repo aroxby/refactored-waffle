5153a46d88127b24c9e4b0194d3678bc038f5028
Andy Roxby <aroxby@users.noreply.github.com>
2023-07-01 17:52:11 -0400

    Move ALL the VPC resources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,106 @@
-locals {
-  eks_cluster_name = "recall-eks"
-  service_account_name = "batch-jobs"
-}
-
 moved {
   from = module.eks.module.vpc.aws_vpc.this[0]
   to   = module.vpc.module.vpc.aws_vpc.this[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_eip.nat[0]
+  to   = module.vpc.module.vpc.aws_eip.nat[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_internet_gateway.this[0]
+  to   = module.vpc.module.vpc.aws_internet_gateway.this[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_nat_gateway.this[0]
+  to   = module.vpc.module.vpc.aws_nat_gateway.this[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_route_table_association.private[0]
+  to   = module.vpc.module.vpc.aws_route_table_association.private[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_route_table_association.private[1]
+  to   = module.vpc.module.vpc.aws_route_table_association.private[1]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_route_table_association.private[2]
+  to   = module.vpc.module.vpc.aws_route_table_association.private[2]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_route_table_association.public[0]
+  to   = module.vpc.module.vpc.aws_route_table_association.public[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_route_table_association.public[1]
+  to   = module.vpc.module.vpc.aws_route_table_association.public[1]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_route_table_association.public[2]
+  to   = module.vpc.module.vpc.aws_route_table_association.public[2]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_route_table.private[0]
+  to   = module.vpc.module.vpc.aws_route_table.private[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_route_table.public[0]
+  to   = module.vpc.module.vpc.aws_route_table.public[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_route.private_nat_gateway[0]
+  to   = module.vpc.module.vpc.aws_route.private_nat_gateway[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_route.public_internet_gateway[0]
+  to   = module.vpc.module.vpc.aws_route.public_internet_gateway[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_subnet.private[0]
+  to   = module.vpc.module.vpc.aws_subnet.private[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_subnet.private[1]
+  to   = module.vpc.module.vpc.aws_subnet.private[1]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_subnet.private[2]
+  to   = module.vpc.module.vpc.aws_subnet.private[2]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_subnet.public[0]
+  to   = module.vpc.module.vpc.aws_subnet.public[0]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_subnet.public[1]
+  to   = module.vpc.module.vpc.aws_subnet.public[1]
+}
+
+moved {
+  from = module.eks.module.vpc.aws_subnet.public[2]
+  to   = module.vpc.module.vpc.aws_subnet.public[2]
+}
+
+locals {
+  eks_cluster_name = "recall-eks"
+  service_account_name = "batch-jobs"
 }
 
 module "vpc" {
